7a301a994fce225a9c6b44412be9bfd674d5955c
mariz2205 <mariz2205>
2025-07-12 10:00:12 +0800

    views appsdev

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace views_appsdev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
